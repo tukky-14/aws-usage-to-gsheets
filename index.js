147fbcf418b8ba836d7fce7e9ca179ea47d7c9b5
tukky-14 <bigoonage0104@gmail.com>
2023-02-25 11:15:12 +0900

    フォルダ構成を見直し

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // GetCostAndUsage
 // https://docs.aws.amazon.com/aws-cost-management/latest/APIReference/API_GetCostAndUsage.html
-// GoogleSpreadsheet
-// https://theoephraim.github.io/node-google-spreadsheet/#/classes/google-spreadsheet
 
 const AWS = require('aws-sdk');
 const ce = new AWS.CostExplorer({ region: 'us-east-1' });
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const { month, monthStart, today } = require('./date.js');
+const { monthStart, today } = require('./lib/date.js');
+const { writeMonthlyCostDataToSheet } = require('./lib/sheet.js');
 
 exports.handler = async (event, context) => {
+    // 料金の取得方法
     const params = {
         TimePeriod: {
             Start: monthStart,
@@ -27,6 +26,7 @@ exports.handler = async (event, context) => {
 
     try {
         const results = await ce.getCostAndUsage(params).promise();
+        // スプレッドシートに書き込むために整形
         const costDataArray = results.ResultsByTime.map((result) => {
             return {
                 timePeriod: result.TimePeriod.Start,
@@ -40,63 +40,7 @@ exports.handler = async (event, context) => {
         });
         console.log(costDataArray);
 
-        const response = await writeCostDataToSheet(costDataArray);
-        return response;
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-};
-
-/**
- * Googleスプレッドシートへデータを書き込む
- * @param {*} costDataArray
- * @returns
- */
-const writeCostDataToSheet = async (costDataArray) => {
-    try {
-        const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
-        await doc.useServiceAccountAuth({
-            client_email: process.env.GOOGLE_SHEET_CLIENT_EMAIL || '',
-            private_key: (process.env.GOOGLE_SHEET_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
-        });
-        await doc.loadInfo();
-
-        // シートが存在しない場合は作成する
-        let sheet;
-        const sheetExists = doc.sheetsByTitle[month] !== undefined;
-        if (sheetExists) {
-            sheet = doc.sheetsByTitle[month];
-        } else {
-            sheet = await doc.addSheet({
-                title: month,
-                headerValues: ['AWS Service', 'Monthly Cost'],
-            });
-        }
-        await sheet.loadCells();
-
-        // 月額料金を書き込み
-        costDataArray[0].Groups.forEach((group, index) => {
-            const cell_A = sheet.getCell(index + 1, 0);
-            const cell_B = sheet.getCell(index + 1, 1);
-            cell_A.value = group.service;
-            cell_B.value = Number(group.cost);
-            cell_B.valueType = 'numberValue';
-        });
-
-        // 合計金額を算出
-        const totalCostCell_A = sheet.getCell(costDataArray[0].Groups.length + 1, 0);
-        totalCostCell_A.value = 'Total Cost';
-        const totalCostCell_B = sheet.getCell(costDataArray[0].Groups.length + 1, 1);
-        totalCostCell_B.value = `=SUM(B2:B${costDataArray[0].Groups.length + 1})`;
-
-        // セル情報を保存
-        await sheet.saveUpdatedCells();
-
-        const response = {
-            statusCode: 200,
-            body: JSON.stringify('Success!'),
-        };
+        const response = await writeMonthlyCostDataToSheet(costDataArray);
         return response;
     } catch (err) {
         console.error(err);
